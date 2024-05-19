c31e9a5a72226cc2c0be679189dc91482ec9c174
Ruvindi <ruvindihulangamuwa@gmail.com>
2024-05-19 19:51:20 -0400

    Modified navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,10 +1,51 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import "./Navbar.css"
 
 function Navbar () {
 
-    const handleClick = () => {
-        
+    const [click, setClick] = useState(false);
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [username, setUsername] = useState("");
+    const [email,setEmail] = useState(sessionStorage.getItem("email"));
+    const [showDropdown, setShowDropdown] = useState(false);
+    const handleClick = () => setClick(!click);
+
+    
+    const handleLogout = () => {
+        sessionStorage.removeItem("auth-token");
+        sessionStorage.removeItem("name");
+        sessionStorage.removeItem("email");
+        sessionStorage.removeItem("phone");
+        // remove email phone
+        localStorage.removeItem("doctorData");
+        setIsLoggedIn(false);
+        // setUsername("");
+       
+        // Remove the reviewFormData from local storage
+        for (let i = 0; i < localStorage.length; i++) {
+          const key = localStorage.key(i);
+          if (key.startsWith("reviewFormData_")) {
+            localStorage.removeItem(key);
+          }
+        }
+        setEmail('');
+        window.location.reload();
     }
+
+    const handleDropdown = () => {
+      setShowDropdown(!showDropdown);
+    }
+
+    useEffect(() => { 
+      const storedemail = sessionStorage.getItem("email");
+
+      if (storedemail) {
+            setIsLoggedIn(true);
+            setUsername(storedemail);
+          }
+    }, []);
 
     return (
         <nav>
@@ -30,16 +71,32 @@ function Navbar () {
                 <li className="link">
                     <a href="#">Reviews</a>
                 </li>
-                <li className="link">
-                    <a href="/sign-up">
-                        <button className="btn1">Sign Up</button>
-                    </a>
-                </li>
-                <li className="link">
-                    <a href="/login">
-                        <button className="btn1">Login</button>
-                    </a>
-                </li>
+                {isLoggedIn?(
+                    <>
+                        <li className="link">
+                            Welcome {email.split("@")[0]}
+                        </li>
+                        <li className="link">
+                        <button className="btn2" onClick={handleLogout}>
+                            Logout
+                        </button>
+                        </li>
+                        
+                    </>
+                    ) : (
+                    <>
+                        <li className="link">
+                        <Link to="/sign-up">
+                            <button className="btn1">Sign Up</button>
+                        </Link>
+                        </li>
+                        <li className="link">
+                        <Link to="/login">
+                            <button className="btn1">Login</button>
+                        </Link>
+                        </li>
+                    </>
+                )}
             </ul>
         </nav>
     )
--- src/Components/Sign_Up/Sign_Up.js
+++ src/Components/Sign_Up/Sign_Up.js
@@ -1,19 +1,73 @@
+import React, { useState } from 'react';
 import './Sign_Up.css'
+import { Link, useNavigate } from 'react-router-dom';
+import { API_URL } from '../../config';
 
-function Sign_Up() {
+const Sign_Up = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [password, setPassword] = useState('');
+    const [showerr, setShowerr] = useState('');
+
+    const navigate = useNavigate();
+
+    const register = async (e) => {
+        e.preventDefault();
+
+        // API Call
+        const response = await fetch(`${API_URL}/api/auth/register`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                name: name,
+                email: email,
+                password: password,
+                phone: phone,
+
+            }),
+        });
+
+        const json = await response.json();
+
+        if (json.authtoken) {
+            sessionStorage.setItem("auth-token", json.authtoken);
+            sessionStorage.setItem("name", name);
+            // phone and email
+            sessionStorage.setItem("phone", phone);
+            sessionStorage.setItem("email", email);
+            // Redirect to home page
+            navigate("/");   //on directing to home page you need to give logic to change login and signup buttons with name of the user and logout button where you have implemented Navbar functionality
+            window.location.reload();
+        } else {
+            if (json.errors) {
+                for (const error of json.errors) {
+                    setShowerr(error.msg);
+                }
+            } else {
+                setShowerr(json.error);
+            }
+        }
+    };
+
     return (
-        <div className="container" style={{marginTop: "5%"}}>
+        <div className="container" style={{marginTop:'5%'}}>
             <div className="signup-grid">
                 <div className="signup-text">
                     <h1>Sign Up</h1>
-
                 </div>
                 <div className="signup-text1">
-                    Already a member? <span><a href="../Login/Login.html" style={{color: "#2190FF"}}> Login</a></span>
+                    Already a member? <span><a href="/login" style={{color: "#2190FF"}}> Login</a></span>
                 </div>
                 <div className="signup-form">
-                    <form>
-
+                    <form method="POST" onSubmit={register}>
+                        <div className="form-group">
+                                <label htmlFor="email">Email</label>
+                                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" name="email" id="email" className="form-control" placeholder="Enter your email" aria-describedby="helpId" />
+                                {showerr && <div className="err" style={{ color: 'red' }}>{showerr.toString()}</div>}
+                        </div>
                         <div className="form-group">
                             <label for="name">Role</label>
                             <select name="name" id="name" required className="form-control" placeholder="Select your role" aria-describedby="helpId" >
@@ -24,23 +78,17 @@ function Sign_Up() {
 
                         <div className="form-group">
                             <label for="name">Name</label>
-                            <input type="text" name="name" id="name" required className="form-control" placeholder="Enter your name" aria-describedby="helpId" />
+                            <input type="text" name="name" onChange={(e) => setName(e.target.value)} id="name" required className="form-control" placeholder="Enter your name" aria-describedby="helpId" />
                         </div>
 
                         <div className="form-group">
                             <label for="phone">Phone</label>
-                            <input type="tel" name="phone" id="phone" required className="form-control" placeholder="Enter your phone number" aria-describedby="helpId" />
-                        </div>
-
-                        <div className="form-group">
-                            <label for="email">Email</label>
-                            <input type="email" name="email" id="email" required className="form-control" placeholder="Enter your email" aria-describedby="helpId" />
-
+                            <input type="tel" name="phone" onChange={(e) => setPhone(e.target.value)} id="phone" required className="form-control" placeholder="Enter your phone number" aria-describedby="helpId" />
                         </div>
 
                         <div className="form-group">
                             <label for="password">Password</label>
-                            <input name="password" id="password" required className="form-control" placeholder="Enter your password" aria-describedby="helpId" />
+                            <input name="password" id="password" onChange={(e) => setPassword(e.target.value)} required className="form-control" placeholder="Enter your password" aria-describedby="helpId" />
                         </div>
 
                         <div className="btn-group">
@@ -50,8 +98,9 @@ function Sign_Up() {
                     </form>
                 </div>
             </div>
-        </div>
-    )
+         </div>
+ //Sign up role is not stored in database. You can apply logic for this according to your react code.
+    );
 }
 
 export default Sign_Up;
