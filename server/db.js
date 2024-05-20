0dd17c2c92c07aa3219762d275f5a814dc53b56a
Ruvindi <ruvindihulangamuwa@gmail.com>
2024-05-20 13:05:42 -0400

    Added build folder

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:MTQ5OTAtcnV2aW5k@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:Mjc4MC1ydXZpbmRp@127.0.0.1:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
